94bc3aa2069aae01b3991a064551539414cbe3ca
jmccallister93 <jaredmccallister93@gmail.com>
2023-10-09 01:01:03 -0700

    added checkboxes for paths and trees

--- battlemap_painter/src/components/CanvasPainter.tsx
+++ battlemap_painter/src/components/CanvasPainter.tsx
@@ -30,6 +30,8 @@ interface Props {
 const CanvasPainter: React.FC<Props> = ({ width, height }) => {
   const canvasRef = useRef<any>(null);
   const [seed, setSeed] = useState(Math.random() * 1000); // Add this line
+  const [showTrees, setShowTrees] = useState(true); // New state for tree visibility
+  const [showPath, setShowPath] = useState(true);
 
   const rerender = () => {
     setSeed(Math.random() * 1000); // Update the seed to trigger a re-render with new noise
@@ -56,6 +58,7 @@ const CanvasPainter: React.FC<Props> = ({ width, height }) => {
       let tilePathImg02: p5.Image;
       let dirtPathImg: p5.Image;
 
+      //   Preload images
       p.preload = () => {
         dirtImg01 = p.loadImage(dirt01);
         dirtImg03 = p.loadImage(dirt03);
@@ -76,7 +79,7 @@ const CanvasPainter: React.FC<Props> = ({ width, height }) => {
         tilePathImg02 = p.loadImage(tilePath02);
         dirtPathImg = p.loadImage(dirtPath01);
       };
-
+      //   Setup canvas
       p.setup = () => {
         p.createCanvas(width, height);
         p.noLoop();
@@ -87,14 +90,16 @@ const CanvasPainter: React.FC<Props> = ({ width, height }) => {
       const tileSize = 60;
       //   Tile size
       const imgSize = tileSize * 4;
-
+      //   Is the path horizonal or vertical
       let isHorizontalPath = Math.random() > 0.5;
       let pathPos = isHorizontalPath
         ? Math.floor(p.random(0, height / tileSize)) * tileSize
         : Math.floor(p.random(0, width / tileSize)) * tileSize;
 
+      // Draw everything
       p.draw = () => {
         p.background(255);
+        // Draw tiles
         for (let y = 0; y < p.height; y += imgSize) {
           for (let x = 0; x < p.width; x += imgSize) {
             const noiseValue = p.noise(x * 0.05, y * 0.05);
@@ -104,7 +109,7 @@ const CanvasPainter: React.FC<Props> = ({ width, height }) => {
             p.translate(x + imgSize / 2, y + imgSize / 2);
             // p.rotate(p.radians(rotation));
 
-            p.image(dirtImg05, -imgSize / 2, -imgSize / 2, imgSize, imgSize);
+            p.image(dirtImg04, -imgSize / 2, -imgSize / 2, imgSize, imgSize);
 
             p.pop();
           }
@@ -112,42 +117,53 @@ const CanvasPainter: React.FC<Props> = ({ width, height }) => {
         const pathSize = tileSize * 0.5; // Adjust as needed to make the path smaller
         const treeSize = tileSize * 0.5; // Adjust as needed to make the trees smaller
         // Draw path tiles over the base tiles
-        if (isHorizontalPath) {
-          for (let x = 0; x < p.width; x += tileSize) {
-            p.push(); // Add this line to isolate the rotation transformation
-            p.translate(x + tileSize / 2, pathPos + tileSize / 2); // Adjust the origin for rotation
-            p.rotate(p.radians(90)); // Rotate the coordinate system by 90 degrees
-            p.image(
-              tilePathImg02,
-              -tileSize / 2,
-              -tileSize / 2,
-              tileSize,
-              tileSize
-            ); // Draw the image at the new origin
-            p.pop(); // Add this line to restore the original coordinate system
-          }
-        } else {
-          for (let y = 0; y < p.height; y += tileSize) {
-            p.image(tilePathImg02, pathPos, y, tileSize, tileSize);
+        if (showPath) {
+          if (isHorizontalPath) {
+            for (let x = 0; x < p.width; x += tileSize) {
+              p.push(); // Add this line to isolate the rotation transformation
+              p.translate(x + tileSize / 2, pathPos + tileSize / 2); // Adjust the origin for rotation
+              p.rotate(p.radians(90)); // Rotate the coordinate system by 90 degrees
+              p.image(
+                tilePathImg02,
+                -tileSize / 2,
+                -tileSize / 2,
+                tileSize,
+                tileSize
+              ); // Draw the image at the new origin
+              p.pop(); // Add this line to restore the original coordinate system
+            }
+          } else {
+            for (let y = 0; y < p.height; y += tileSize) {
+              p.image(tilePathImg02, pathPos, y, tileSize, tileSize);
+            }
           }
         }
 
         const trees = [treeImg01, treeImg02, treeImg03];
         // Draw all trees
-        for (let y = 0; y < p.height; y += tileSize) {
-          for (let x = 0; x < p.width; x += tileSize) {
-            if (Math.random() < 0.1) {
-              // Adjusted to 10% for more trees, change as needed
-              const treeImg = p.random(trees); // Choose a random tree image from the array
-              const treeScale = 0.5 + Math.random() * 1.5; // Random scale between 0.5 and 2
-              const treeRotation = Math.floor(Math.random() * 4) * 90; // Random rotation
+        if (showTrees) {
+          for (let y = 0; y < p.height; y += tileSize) {
+            for (let x = 0; x < p.width; x += tileSize) {
+              if (Math.random() < 0.03) {
+                // This condition checks if the tree is on the path
+                if (
+                  (isHorizontalPath && y === pathPos) ||
+                  (!isHorizontalPath && x === pathPos)
+                ) {
+                  continue; // Skip this iteration to avoid drawing tree on the path
+                }
 
-              p.push();
-              p.translate(x + tileSize / 2, y + tileSize / 2);
-              p.scale(treeScale);
-              p.rotate(p.radians(treeRotation));
-              p.image(treeImg, -treeImg.width / 2, -treeImg.height / 2); // Changed to use the random tree image
-              p.pop();
+                const treeImg = p.random(trees);
+                const treeScale = 0.5 + Math.random() * 1.5;
+                const treeRotation = Math.floor(Math.random() * 4) * 90;
+
+                p.push();
+                p.translate(x + tileSize / 2, y + tileSize / 2);
+                p.scale(treeScale);
+                p.rotate(p.radians(treeRotation));
+                p.image(treeImg, -treeImg.width / 2, -treeImg.height / 2);
+                p.pop();
+              }
             }
           }
         }
@@ -163,7 +179,22 @@ const CanvasPainter: React.FC<Props> = ({ width, height }) => {
       <div className="canvasContianer">
         <div ref={canvasRef}></div>
         <button onClick={rerender}>Rerender Sketch</button>{" "}
-        {/* Add this button */}
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={showTrees}
+              onChange={(e) => setShowTrees(e.target.checked)}
+            /> Show Trees
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              checked={showPath}
+              onChange={(e) => setShowPath(e.target.checked)}
+            /> Show Path
+          </label>
+        </div>
       </div>
     </>
   );
